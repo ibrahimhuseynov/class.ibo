16fb77a227b3e3a8bb55a53fb47b073d50e0b4fc
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-11 07:35:52 +0400

    class lesson

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product prd=new Product();
+            prd.name = "kartof";
+            prd.brandname = "bakikartofu";
+            prd.price = 0.80;
+            Console.WriteLine("name :" + prd.name);
+            Console.WriteLine("brandname :"+prd.brandname);
+            Console.WriteLine("price :"+prd.price);
+            Console.Read();
+        }
+    }
+}
